8cc534b884807b87bdc4cb910d46e29bf925e811
scryptalex <scryptalex@users.noreply.github.com>
2025-10-15 01:19:20 +0300

    Deploy to GitHub Pages

---
+++ _next/static/chunks/app/[locale]/layout-0e324b5928b3f978.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[450],{3809:(e,r,l)=>{"use strict";l.d(r,{default:()=>t});var s=l(3791),o=l(5155);function t(e){let{locale:r,...l}=e;if(!r)throw Error(void 0);return(0,o.jsx)(s.Dk,{locale:r,...l})}},7720:(e,r,l)=>{Promise.resolve().then(l.bind(l,3809))}},e=>{e.O(0,[791,441,255,358],()=>e(e.s=7720)),_N_E=e.O()}]);
